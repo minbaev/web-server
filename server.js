ac69529c2200fa4a4557f3dfac17a53d91c65391
minbaev <minbaev@gmail.com>
2016-06-06 11:55:01 -0700

    One

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var middleware = require('./middleware.js')
 app.use(middleware.logger);
 
 app.get('/about',middleware.requireAuthentication, function(req, res){
-	res.send("About Us!");
+	res.send("All About Us!");
 });
 
 app.use(express.static(__dirname + "/public"));
